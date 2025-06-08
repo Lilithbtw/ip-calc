f25d6d5afbb52d19f42f2066f54e6bfc20eefda2
Lilithbtw <lili.macias@proton.me>
2025-06-08 11:46:46 +0200

    match cases fixed

--- ipv4-check.py
+++ ipv4-check.py
@@ -30,20 +30,18 @@ def main(ip):
 
         final_ip = '{0:08b}'.format(int(frst_ip))[:4]
         match final_ip:
-            case "0000" | "0100":
+            case "0000" | "0001" | "0010" | "0011" |"0100" | "0101" | "0110" | "0111":
                 return "Your IP is class A with a 255.0.0.0 netmask."
-            case "1000":
+            case "1000" | "1001" | "1010" | "1011":
                 return "Your IP is class B with a 255.255.0.0 netmask."
-            case "1100":
+            case "1100" | "1101":
                 return "Your IP is class C with a 255.255.255.0 netmask."
             case "1110":
                 return "Your IP is class D (Multicast)."
             case "1111":
                 return "Your IP is class E (Experimental/Reserved)."
             case _:
-                first_octet = int(frst_ip)
-                if first_octet < 128:
-                    return "Your IP is class A with a 255.0.0.0 netmask."
+                return "Could not determine IP class."
     else:
         return "Your value isn't a valid IP."
 
